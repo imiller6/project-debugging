826bbffad2260f902c32c60c3b21fad2959d4b08
Ian Miller <imiller6@mail.valenciacollege.edu>
2016-03-16 21:29:17 -0400

    debugging complete

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
-def add( first, second):
+def add(first, second):
     print(int(first) + int(second))
 
-def subtract( first, second):
+def subtract(first, second):
     print(int(first) - int(second))
 
-def multiply( first, second):
+def multiply(first, second):
     print(int(first) * int(second))
 
-def divide( first, second):
+def divide(first, second):
     if int(second) == 0:
     	raise Exception("    I'm sorry, I can't divide by zero")
     else:
